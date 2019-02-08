69bbdaa0b35101408ee7a2640dfbd650d588e33d
Frank Lyder Bredland <fl@nyggen.com>
2019-02-08 22:14:55 +0100

    Refactored Canvas into its own file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,10 @@
-import React, {useEffect, forwardRef, useImperativeHandle, createRef} from 'react';
-import './App.css';
+import React, {createRef} from 'react';
 import { Route, withRouter, Switch } from 'react-router-dom';
-
-const Canvas = forwardRef( ({ getImageData }, ref) => {
-	const canvasRef = createRef();
-	let ctx;
-
-	useEffect(() => {
-		ctx = canvasRef.current.getContext("2d");
-		ctx.canvas.width = canvasRef.current.parentElement.scrollWidth;
-		ctx.canvas.height = canvasRef.current.parentElement.scrollHeight;
-		let prev = null;
-
-		const mousedown = e => prev = Object.assign({}, e);
-		canvasRef.current.addEventListener("mousedown", mousedown);
-
-		const mousemove = e => {
-			if(prev == null) return;
-
-			ctx.beginPath();
-			ctx.moveTo(prev.offsetX, prev.offsetY);
-			ctx.lineTo(e.offsetX, e.offsetY);
-			ctx.stroke();
-
-			prev = e;
-		};
-		canvasRef.current.addEventListener("mousemove", mousemove);
-
-		const mouseup = e => prev = null;
- 		canvasRef.current.addEventListener("mouseup", mouseup);
-
-		return () => {
-			canvasRef.removeEventListener("mousedown", mousedown);
-			canvasRef.removeEventListener("mousemove", mousemove);
-			canvasRef.removeEventListener("mouseup",   mouseup);
-		};
-	});
-
-	useImperativeHandle(ref, () => ({
-		getImageData() {
-			return ctx.canvas.toDataURL();
-		}
-	}));
-
-	return <canvas ref={canvasRef} />
-} );
+import './App.css';
+import Canvas from './Canvas';
 
 const Chat = () => {
-	const canvasRef = React.createRef();
+	const canvasRef = createRef();
 
 	return <>
 		<h1> new chat </h1>
